5b94edb2625ec870016356d90d204835cadfb601
jorginfinan <jorgelucasdutra1@gmail.com>
2025-11-19 01:41:48 -0300

    Add files via upload

--- SISTEMA BSX/js/saldo-acumulado.js
+++ SISTEMA BSX/js/saldo-acumulado.js
@@ -157,7 +157,7 @@
       }
   
       const valorComissao = (baseCalculo * comissao) / 100;
-      const aPagar = resultado < 0 ? resultado : valorComissao;
+      const aPagar = resultado - valorComissao;
   
       return {
         coletas,
